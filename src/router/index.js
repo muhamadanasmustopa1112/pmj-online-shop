62c88200155a4a2f60b07d56a1f2a88567a24d6e
muhamadanasmustopa <muhamadanasmustopa1112@gmail.com>
2022-11-23 13:47:28 +0700

    Projek Vue JS

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Product from '../views/Product.vue'
+import ShopingCart from '../views/ShopingCart.vue'
+import Success from '../views/Success.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( /* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/product',
+    name: 'product',
+    component: Product
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: ShopingCart
+  },
+  {
+    path: '/success',
+    name: 'success',
+    component: Success
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroCarousel />
+    <BannerCarousel />
+    <InstaPmj />
+    <PartnerPmj />
+    <FooterPmj />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import BannerCarousel from '@/components/BannerCarousel.vue';
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroCarousel from '@/components/HeroCarousel.vue';
+import InstaPmj from '@/components/InstaPmj.vue';
+import PartnerPmj from '@/components/PartnerPmj.vue';
+import FooterPmj from '@/components/FooterPmj.vue';
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderShayna,
+    HeroCarousel,
+    BannerCarousel,
+    InstaPmj,
+    PartnerPmj,
+    FooterPmj
+}
+}
+</script>
